56c8cb61f511ec145ef95b6629c44151aec931af
Víctor Gómez <vgomez@applaudostudios.dev>
2022-04-03 10:31:30 -0400

    Add coverage, flake8 (2)

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(name='film_rental_system',
+      version='1.0',
+      description='A film rental system on Fast API',
+      author='Victor Gomez',
+      author_email='vgomez@applaudostudios.dev',
+      packages=find_packages(),
+      )
